cea789b983f492283b74b9610cd2836c6b811542
zurfyx <zurfyx@gmail.com>
2017-07-06 17:26:00 +0200

    initialize unique firebase instance

--- src/utils/FirebaseInstance.ts
+++ src/utils/FirebaseInstance.ts
@@ -2,12 +2,13 @@ import * as firebase from 'firebase';
 
 import config from '../config';
 import { FirebaseConfig } from '../models/FirebaseConfig';
+import { genTimeId } from './random';
 
 export class FirebaseInstance {
   firebase: firebase.app.App;
 
   constructor(config: FirebaseConfig) {
-    this.firebase = firebase.initializeApp(config);
+    this.firebase = firebase.initializeApp(config, `api-${genTimeId()}`);
   }
 }
 
